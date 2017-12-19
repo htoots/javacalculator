f31e7cb36472be9666a7dd79add73edc4d4cbe8e
htoots <htoots@ttu.ee>
2017-12-19 23:01:40 +0200

    naming

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,11 @@ public class Main {
 
         //Do loop for keeping the program going for as long as the user wants
         do {
-            int inputchoice = calc.getChoice();                         //Get choice of operation
+            int inputChoice = calc.getChoice();                         //Get choice of operation
             double num1 = calc.getNum1();                               //Get num1(x)
             double num2 = calc.getNum2();                               //Get num2(y)
-            char operator = ui.returnChoice(inputchoice);               //Get char for printAnswer
-            double answer = calc.calculate(num1, num2, inputchoice);    //Get answer
+            char operator = ui.returnChoice(inputChoice);               //Get char for printAnswer
+            double answer = calc.calculate(num1, num2, inputChoice);    //Get answer
 
             ui.printAnswer(num1, num2, answer, operator);               //Print final result
 
