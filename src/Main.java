02e44944a7e48d75cfbd8c8b18b580356cceeced
htoots <htoots@ttu.ee>
2017-12-19 02:54:28 +0200

    Easier read

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,20 @@ import calcui.UI;
 
 public class Main {
     public static void main(String[] args){
+        //Objects
         Calculator calc = new Calculator();
         UI ui = new UI();
-        int inputchoice = calc.getChoice();
-        System.out.println(inputchoice);
-        double num1 = calc.getNum1();
-        double num2 = calc.getNum2();
-        System.out.println(num1 + " " + num2);
-        char operator = ui.returnChoice(inputchoice);
-        double answer = calc.calculate(num1, num2, inputchoice);
+
+        ui.turnOn();                                                //Welcome screen
+        int inputchoice = calc.getChoice();                         //Get choice of operation
+        //System.out.println(inputchoice);                          Debug code
+        double num1 = calc.getNum1();                               //Get num1(x)
+        double num2 = calc.getNum2();                               //Get num2(y)
+        //System.out.println(num1 + " " + num2);                    Debug code
+        char operator = ui.returnChoice(inputchoice);               //Get char for printAnswer
+        double answer = calc.calculate(num1, num2, inputchoice);    //Get answer
 
         //Print final result
-        System.out.println(num1 + " " + operator + " " + num2 + " is " + answer);
+        ui.printAnswer(num1, num2, answer, operator);
     }
 }
